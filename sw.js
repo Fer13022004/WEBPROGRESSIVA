50d59d603efb4a445f57903eaac00d095f388028
ftima13 <utp0156952@alumno.utpuebla.edu.mx>
2025-11-30 22:58:01 -0600

    New

--- sw.js
+++ sw.js
@@ -10,6 +10,7 @@ const urlsToCache = [
   `${basePath}/js/app.js`,
   `${basePath}/js/config.js`,
   `${basePath}/js/sw-register.js`,
+  `${basePath}/js/criterios.js`
   `${basePath}/js/primos-compuestos.js`,
   `${basePath}/manifest.json`,
   `${basePath}/images/icon-192x192.svg`,
@@ -17,6 +18,7 @@ const urlsToCache = [
   `${basePath}/pages/primos-compuestos.html`,
   `${basePath}/pages/multiplos.html`,
   `${basePath}/pages/primos.html`,
+  `${basePath}/pages/criterios.html,
 ];
 
 // Instalación - Cachear archivos
